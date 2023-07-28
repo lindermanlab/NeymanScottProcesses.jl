191cedb1211ab0ad17d07a33dfb00cb5ab5698b6
degleris1 <degleris1@gmail.com>
2023-07-28 15:16:30 -0700

    Added environment for neuroscience experiments

--- setup.jl
+++ setup.jl
@@ -4,10 +4,14 @@ using Pkg
 Pkg.activate(@__DIR__)
 Pkg.instantiate()
 
-# Install experiment environments
+# Install synthetic experiment environments
 Pkg.activate(joinpath(@__DIR__, "experiments", "dpm_limit"))
 Pkg.instantiate()
 
-# Install experiment environments
+# Install embassy experiment environments
 Pkg.activate(joinpath(@__DIR__, "experiments", "embassy"))
+Pkg.instantiate()
+
+# Install neural experiment environments
+Pkg.activate(joinpath(@__DIR__, "ppseq"))
 Pkg.instantiate()
